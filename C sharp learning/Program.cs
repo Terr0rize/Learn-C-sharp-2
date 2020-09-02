5b808c9ab3dd9ae7d860cbc98445e02afab2ed48
kitsune <57214464+Terr0rize@users.noreply.github.com>
2020-09-02 16:43:48 +0400

    Add files via upload

---
+++ C sharp learning/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace C_sharp_learning
+{
+    class MainClass
+    {
+        static void Main(string[] args) //метод срабатывает при старте программы
+        {
+            double num;
+            Console.WriteLine("How old are you?");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            if (num > 18.23d && num < 50)
+            {
+                Console.WriteLine("You are older than 18.23");
+            }
+            else if(num < 10)
+            {
+                Console.WriteLine("Under 10");
+            }
+            else
+            { 
+                Console.WriteLine("Your little Boy:)"); 
+            }
+            string name = num == 15 ? "Airat" : "Ramis Pizdaliz";
+            Console.WriteLine(name);
+            Console.ReadKey();
+        }
+    }
+}
